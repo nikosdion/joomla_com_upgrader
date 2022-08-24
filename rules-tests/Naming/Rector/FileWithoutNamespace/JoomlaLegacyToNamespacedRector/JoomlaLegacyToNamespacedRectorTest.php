6689c3b4ed5627c7b894ac7487e4fa2692f74111
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-08-24 16:31:28 +0300

    Basic tests

--- rules-tests/Naming/Rector/FileWithoutNamespace/JoomlaLegacyToNamespacedRector/JoomlaLegacyToNamespacedRectorTest.php
+++ rules-tests/Naming/Rector/FileWithoutNamespace/JoomlaLegacyToNamespacedRector/JoomlaLegacyToNamespacedRectorTest.php
@@ -8,24 +8,31 @@ use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 final class JoomlaLegacyToNamespacedRectorTest extends AbstractRectorTestCase
 {
-    /**
-     * @dataProvider provideData()
-     */
-    public function test(\Symplify\SmartFileSystem\SmartFileInfo $fileInfo): void
-    {
-        $this->doTestFileInfo($fileInfo);
-    }
+	protected function setUp(): void
+	{
+		require_once __DIR__ . '/../../../../../override/Symplify/EasyTesting/StaticFixtureSplitter.php';
 
-    /**
-     * @return \Iterator<\Symplify\SmartFileSystem\SmartFileInfo>
-     */
-    public function provideData(): \Iterator
-    {
-        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
-    }
+		parent::setUp();
+	}
 
-    public function provideConfigFilePath(): string
-    {
-        return __DIR__ . '/config/configured_rule.php';
-    }
+	public function provideConfigFilePath(): string
+	{
+		return __DIR__ . '/config/configured_rule.php';
+	}
+
+	/**
+	 * @return \Iterator<\Symplify\SmartFileSystem\SmartFileInfo>
+	 */
+	public function provideData(): \Iterator
+	{
+		return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+	}
+
+	/**
+	 * @dataProvider provideData()
+	 */
+	public function test(\Symplify\SmartFileSystem\SmartFileInfo $fileInfo): void
+	{
+		$this->doTestFileInfo($fileInfo);
+	}
 }
--- rules-tests/Naming/Rector/FileWithoutNamespace/JoomlaLegacyToNamespacedRector/config/configured_rule.php
+++ rules-tests/Naming/Rector/FileWithoutNamespace/JoomlaLegacyToNamespacedRector/config/configured_rule.php
@@ -1,9 +1,20 @@
 <?php
-
 declare (strict_types=1);
+
 namespace RectorPrefix202208;
 
 use Rector\Config\RectorConfig;
+use Rector\Naming\Config\JoomlaLegacyPrefixToNamespace;
+use Rector\Naming\Rector\FileWithoutNamespace\JoomlaLegacyToNamespacedRector;
+
 return static function (RectorConfig $rectorConfig) : void {
-    $rectorConfig->rule(\Rector\Naming\Rector\FileWithoutNamespace\JoomlaLegacyToNamespacedRector::class);
+	$rectorConfig->ruleWithConfiguration(
+		JoomlaLegacyToNamespacedRector::class,
+		[
+			new JoomlaLegacyPrefixToNamespace('Example', '\\Acme\\Example', [
+				'Example',
+				'ExampleFoobar',
+			])
+		]
+	);
 };
